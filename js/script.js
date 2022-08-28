8cf7e0f93abd3f7aba18728b3bc91fca39d539d3
Ansha Prashanth <anshaprashanth@gmail.com>
2022-08-28 09:51:11 +0530

    changes made to student dashboard

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $('#autoWidth').lightSlider({
+       autoWidth:true,
+       loop:true,
+       onSliderLoad: function() {
+           $('#autoWidth').removeClass('cS-hidden');
+       }
+   });
+ });
+
+ $(document).singlePageNav({filter: ':not(.external)'});
